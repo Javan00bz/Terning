61e3e304b19a87e49ca32db874b38a8fe74f731d
Zeydel <32457337+Zeydel@users.noreply.github.com>
2017-10-09 13:30:08 +0200

    Add files via upload

--- Dice.java
+++ Dice.java
@@ -3,15 +3,15 @@ package terning;
 public class Dice {
 	// roll the die and return the value (1-6)
 	public int roll() {
-		float d1=(float)Math.random();     // 0-1
-		float d2=d1*5;                     // 0-5
-		int d3=Math.round(d2);             // 0-5 integer
-		return d3 + 1;                     // 1-6		
+		int dice = (int)(Math.random()*6+1);
+		return dice;
 	}
 	
 	// roll the die n times and print the values
+	
+	
 	public void rollMultiple(int n) {
-		for (int i=1; i<n; i++) {
+		for (int i=1; i<=n; i++) {
 			System.out.print(roll() + " ");
 		}		
 	}
